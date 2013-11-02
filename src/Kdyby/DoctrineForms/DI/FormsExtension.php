df732dfc57fcfc72a2e80f0cc56f83ad8a0ca664
Filip Procházka <filip@prochazka.su>
2013-11-02 13:21:25 +0100

    DoctrineForms separated from Kdyby/Doctrine

---
+++ src/Kdyby/DoctrineForms/DI/FormsExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Forms\DI;
+
+use Kdyby;
+use Nette;
+use Nette\PhpGenerator as Code;
+
+
+
+if (!class_exists('Nette\DI\CompilerExtension')) {
+	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+}
+
+if (isset(Nette\Loaders\NetteLoader::getInstance()->renamed['Nette\Configurator']) || !class_exists('Nette\Configurator')) {
+	unset(Nette\Loaders\NetteLoader::getInstance()->renamed['Nette\Configurator']); // fuck you
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
+}
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class FormsExtension extends Nette\DI\CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('entityFormMapper'))
+			->setClass('Kdyby\Doctrine\Forms\EntityFormMapper')
+			->setInject(FALSE);
+	}
+
+
+
+	public static function register(Nette\Configurator $configurator)
+	{
+		$configurator->onCompile[] = function ($config, Nette\DI\Compiler $compiler) {
+			$compiler->addExtension('doctrineForms', new FormsExtension());
+		};
+	}
+
+}
+
